d5e917a2e0c3df6725fd1378f9bcc1faee2a1f68
Ashley Chou <ashchou3@gmail.com>
2012-11-12 11:50:30 -0500

    Including scroll component

--- TabbedApp/TabbedAppAppDelegate.h
+++ TabbedApp/TabbedAppAppDelegate.h
@@ -8,8 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
+@class MyViewController;
+
 @interface TabbedAppAppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
+@property (nonatomic, retain) MyViewController *myViewController;
 
 @end
